78547ffe81f8d980a775477cdeef2f425b836550
Jamila Alsayed Kassem <51366599+JamilaKassem99@users.noreply.github.com>
2023-01-09 13:21:47 +0100

    Delete rl-autoscaler-k8s directory

--- rl-autoscaler-k8s/gym_k8s_real/setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup
-
-setup(name='gym_k8s_real',
-      version='0.0.1',
-      install_requires=['gym','requests','pint','pyyaml']  # And any other dependencies foo needs
-      #dependency_links=['http://github.com/kubernetes-client/repo/tarball/master#egg=python-12.0.0-snapshot']
-)
